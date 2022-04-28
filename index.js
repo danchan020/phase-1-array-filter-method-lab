03b0f6e72e35a99c5d8c4a785c4dadce45a5c1c2
Daniel <danchan020@gmail.com>
2022-04-28 01:18:10 -0700

    commit tree 0623b17f3bac9e54f72d469e1e6d68141dc8268c

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+// function findMatching(drivers,name){
+//     return drivers.filter(function(driver){
+//         return 
+//     }
+// }
+
+function findMatching(drivers, string) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === string.toLowerCase() 
+    })
+}
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(function (driver){
+        return driver 
+    })
+}
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+
+
+function matchName(driverObj,string){
+    return driverObj.filter(function(driver){
+        return driver.name === string
+    })
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+//   function matchName(drivers, argument) {
+//     return drivers.filter(function (driver) { return driver.name === argument })
